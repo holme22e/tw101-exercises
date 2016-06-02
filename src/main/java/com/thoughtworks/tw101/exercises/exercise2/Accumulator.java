55e248f770f0728d92ad8a515ebe21d00ae40c4c
Ella Holmes <holme22e@mtholyoke.edu>
2016-06-02 10:53:33 -0400

    exercise 2 complete

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    public static int incrementCounter = 0;
+
+    public void increment(){
+        incrementCounter++;
+    }
+
+    public void total(){
+        System.out.println(incrementCounter);
+    }
 }
